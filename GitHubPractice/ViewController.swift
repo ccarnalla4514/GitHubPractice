4c8699bba89dca016ef4a3e024881315c2258a7f
Christian Carnalla <ccarnalla4514@stu.d214.org>
2020-10-26 15:33:04 -0500

    Added textfield, label and button to storyboard. Added code to view controller to show what was entered.

--- GitHubPractice/ViewController.swift
+++ GitHubPractice/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenButtonPressed(_ sender: Any) {
+        let data = textfield.text!
+        label.text  = data
+    }
+    
+    
 }
 
